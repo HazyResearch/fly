839f90e16d084303347b6bd5c97778170c25b7a0
Tri Dao <tridpq@gmail.com>
2022-07-20 22:33:40 -0400

    Update seq.py

--- src/tasks/seq.py
+++ src/tasks/seq.py
@@ -7,10 +7,13 @@ from torchmetrics import MetricCollection
 
 from einops import rearrange
 
+from omegaconf import OmegaConf
+
 from src.utils.utils import get_logger
 from src.optim.param_grouping import group_parameters_for_optimizer
+from src.utils.checkpoint import load_checkpoint
 
-log = get_logger(__name__)
+logger = get_logger(__name__)
 
 
 class SequenceModel(LightningModule):
@@ -25,35 +28,36 @@ class SequenceModel(LightningModule):
         self.cfg = cfg
         self.model_cfg = model_cfg or self.cfg.model
 
-        # Init lightning datamodule
-        log.info(f"Instantiating datamodule <{cfg.datamodule._target_}>")
+        self.instantiate_datamodule()
+        self.instantiate_model()
+        self.warmstart()
+        self.instantiate_loss()
+        self.instantiate_metrics()
+
+    def instantiate_datamodule(self):
+        logger.info(f"Instantiating datamodule <{self.cfg.datamodule._target_}>")
         # Calling this self.datamodule will mess with PL since it also assigns self.datamodule
-        self._datamodule: LightningDataModule = hydra.utils.instantiate(cfg.datamodule)
+        self._datamodule: LightningDataModule = hydra.utils.instantiate(self.cfg.datamodule)
         self._datamodule.prepare_data()
         self._datamodule.setup()
 
+    def instantiate_model(self):
         if hasattr(self._datamodule, 'num_classes'):
             self.model_cfg.num_classes = self._datamodule.num_classes
         if (hasattr(self._datamodule, 'vocab_size')
             and self.model_cfg.get('embedding_cfg', None) is not None):
             self.model_cfg.embedding_cfg.num_embeddings = self._datamodule.vocab_size
-        log.info(f"Instantiating model <{self.model_cfg._target_}>")
-        self.model = hydra.utils.instantiate(self.model_cfg, _recursive_=False)
+        logger.info(f"Instantiating model <{self.model_cfg._target_}>")
+        recursive = getattr(self.model_cfg, '_recursive_', False)
+        self.model = hydra.utils.instantiate(self.model_cfg, _recursive_=recursive)
 
-        # Mixup / Cutmix
-        if hasattr(self.cfg.train, 'mixup'):
-            if hasattr(self._datamodule, 'num_classes'):
-                self.cfg.train.mixup.num_classes = self._datamodule.num_classes
-            self.mixup = hydra.utils.instantiate(self.cfg.train.mixup)
-        else:
-            self.mixup = None
-
-        # loss function
+    def instantiate_loss(self):
         loss_fn_cfg = self.cfg.train.get('loss_fn', {'_target_': 'torch.nn.CrossEntropyLoss'})
         self.loss_fn = hydra.utils.instantiate(loss_fn_cfg)
         loss_fn_val_cfg = self.cfg.train.get('loss_fn_val', loss_fn_cfg)
         self.loss_fn_val = hydra.utils.instantiate(loss_fn_val_cfg)
 
+    def instantiate_metrics(self):
         # use separate metric instance for train, val and test step
         # to ensure a proper reduction over the epoch
         if 'eval' in self.cfg and 'metrics' in self.cfg.eval:
@@ -66,22 +70,16 @@ class SequenceModel(LightningModule):
         self.val_metrics = metrics.clone(prefix='val/')
         self.test_metrics = metrics.clone(prefix='test/')
 
-    def setup(self, stage=None):
-        # Init model, but not if we're testing
-        if stage == 'test':
-            return
-        if hasattr(self._datamodule, 'num_classes'):
-            self.model_cfg.num_classes = self._datamodule.num_classes
-        if (hasattr(self._datamodule, 'vocab_size')
-            and self.model_cfg.get('embedding_cfg', None) is not None):
-            self.model_cfg.embedding_cfg.num_embeddings = self._datamodule.vocab_size
-
-        # BC init model only when it's None
-        if self.model is None:
-            log.info(f"Instantiating model <{self.model_cfg._target_}>")
-            self.model = hydra.utils.instantiate(self.model_cfg, _recursive_=False)
-        else:
-            log.info("Model Already initialized")
+    def warmstart(self):
+        if self.cfg.train.get('warmstart', None) is not None:
+            logger.info(f"Warm-starting with weights from {self.cfg.train.warmstart.path}")
+            strict = self.cfg.train.warmstart.get('strict', True)
+            state_dict = load_checkpoint(self.cfg.train.warmstart.path)
+            if self.cfg.train.warmstart.get('post_process', None) is not None:
+                state_dict = hydra.utils.instantiate(self.cfg.train.warmstart.post_process,
+                                                     state_dict)
+            load_return = self.model.load_state_dict(state_dict, strict=False)
+            logger.info(load_return)
 
     def forward(self, *args, **kwargs):
         return self.model(*args, **kwargs)
@@ -92,16 +90,14 @@ class SequenceModel(LightningModule):
         except ValueError:
             x, y = batch
             lengths = None
-        if is_train and self.mixup is not None:
-            x, y = self.mixup(x, y)
-        targets = y.argmax(dim=-1) if is_train and self.mixup is not None else y  # In case of Mixup
         output = self.forward(x) if lengths is None else self.forward(x, lengths=lengths)
         loss = self.loss_fn(output, y) if is_train else self.loss_fn_val(output, y)
-        return loss, output, targets
+        return loss, output, y
 
     def shared_step(self, batch: Any, batch_idx: int, phase='train'):
         loss, output, targets = self.step(batch, is_train=(phase == 'train'))
-        metrics = getattr(self, f'{phase}_metrics')(output, targets)
+        with torch.no_grad():
+            metrics = getattr(self, f'{phase}_metrics')(output, targets)
         self.log(f"{phase}/loss", loss, on_step=False, on_epoch=True, prog_bar=False, sync_dist=True)
         self.log_dict(metrics, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
         return {"loss": loss, "output": output, "targets": targets}
@@ -123,6 +119,14 @@ class SequenceModel(LightningModule):
             # parameters = self.model.parameters()
             parameters = self.parameters() # [21-09-08] AG: this will train task specific parameters such as Retrieval head for AAN
         optimizer = hydra.utils.instantiate(self.cfg.train.optimizer, parameters)
+
+        # Log optimizer info
+        for i, g in enumerate(optimizer.param_groups):
+            ntensors = len(g['params'])
+            nparams = sum(p.numel() for p in g['params'])
+            hparams = {k: v for k, v in g.items() if k != 'params'}
+            logger.info(f'Optimizer group {i}: {ntensors} tensors, {nparams} parameters, {hparams}')
+
         if 'scheduler' not in self.cfg.train:
             return optimizer
         else:
@@ -145,90 +149,19 @@ class SequenceDualModel(SequenceModel):
 
 class SequenceLMModel(SequenceModel):
 
-    def __init__(self, cfg, model_cfg=None):
-        """If model_cfg is passed, it will take precedence over cfg.model
-        """
-        # For some reason calling LightningModule.__init__(self) causes error when I call
-        # self.save_hyperparameters(cfg)
-        super(SequenceModel, self).__init__()
-        # this line ensures params passed to LightningModule will be saved to ckpt
-        # it also allows to access params with 'self.hparams' attribute
-        self.save_hyperparameters(cfg)
-        self.cfg = cfg
-        self.model_cfg = model_cfg or self.cfg.model
-
-        # Init lightning datamodule
-        log.info(f"Instantiating datamodule <{cfg.datamodule._target_}>")
-        # Calling this self.datamodule will mess with PL since it also assigns self.datamodule
-        self._datamodule: LightningDataModule = hydra.utils.instantiate(cfg.datamodule)
-        self._datamodule.prepare_data()
-        self._datamodule.setup()
-
-        if hasattr(self._datamodule, 'num_classes'):
-            self.model_cfg.num_classes = self._datamodule.num_classes
+    def instantiate_model(self):
         if (hasattr(self._datamodule, 'vocab_size')
             and self.model_cfg.get('embedding_cfg', None) is not None):
             self.model_cfg.embedding_cfg.num_embeddings = self._datamodule.vocab_size
-        log.info(f"Instantiating model <{self.model_cfg._target_}>")
+        logger.info(f"Instantiating model <{self.model_cfg._target_}>")
+        # Huggingface models need the config object to be instantiated first
         config = hydra.utils.instantiate(self.model_cfg.pop('config'), _recursive_=False)
         self.model = hydra.utils.instantiate(self.model_cfg, config, _recursive_=False)
 
-        # Mixup / Cutmix
-        if hasattr(self.cfg.train, 'mixup'):
-            if hasattr(self._datamodule, 'num_classes'):
-                self.cfg.train.mixup.num_classes = self._datamodule.num_classes
-            self.mixup = hydra.utils.instantiate(self.cfg.train.mixup)
-        else:
-            self.mixup = None
-
-        # loss function
-        loss_fn_cfg = self.cfg.train.get('loss_fn', {'_target_': 'torch.nn.CrossEntropyLoss'})
-        self.loss_fn = hydra.utils.instantiate(loss_fn_cfg)
-        loss_fn_val_cfg = self.cfg.train.get('loss_fn_val', loss_fn_cfg)
-        self.loss_fn_val = hydra.utils.instantiate(loss_fn_val_cfg)
-
-        # use separate metric instance for train, val and test step
-        # to ensure a proper reduction over the epoch
-        if 'eval' in self.cfg and 'metrics' in self.cfg.eval:
-            metrics_cfg = self.cfg.eval.metrics
-        else:
-            metrics_cfg = {'acc': {'_target_': 'torchmetrics.Accuracy'}}
-        metrics = MetricCollection({name: hydra.utils.instantiate(cfg)
-                                    for name, cfg in metrics_cfg.items()})
-        self.train_metrics = metrics.clone(prefix='train/')
-        self.val_metrics = metrics.clone(prefix='val/')
-        self.test_metrics = metrics.clone(prefix='test/')
-
-    def setup(self, stage=None):
-        # Init model, but not if we're testing
-        if stage == 'test':
-            return
-        if hasattr(self._datamodule, 'num_classes'):
-            self.model_cfg.num_classes = self._datamodule.num_classes
-        if (hasattr(self._datamodule, 'vocab_size')
-            and self.model_cfg.get('embedding_cfg', None) is not None):
-            self.model_cfg.embedding_cfg.num_embeddings = self._datamodule.vocab_size
-
-        # BC init model only when it's None
-        if self.model is None:
-            log.info(f"Instantiating model <{self.model_cfg._target_}>")
-            config = hydra.utils.instantiate(self.model_cfg.pop('config'), _recursive_=False)
-            self.model = hydra.utils.instantiate(self.model_cfg, config, _recursive_=False)
-        else:
-            log.info("Model Already initialized")
-
     def step(self, batch: Any, is_train=True):
-        x, y = batch['input_ids'], batch['labels']
-        output = self.forward(input_ids=x, attention_mask=batch['attention_mask']).logits
-        # Need to shift since huggingface has input_ids == labels
-        output = rearrange(output[..., :-1, :], '... C -> (...) C')
-        y = rearrange(y[..., 1:], '... -> (...)')
+        x, y = batch
+        output = self.forward(x).logits
+        output = rearrange(output, '... C -> (...) C')
+        y = rearrange(y, '... -> (...)')
         loss = self.loss_fn(output, y) if is_train else self.loss_fn_val(output, y)
         return loss, output, y
-
-    def shared_step(self, batch: Any, batch_idx: int, phase='train'):
-        loss, output, targets = self.step(batch, is_train=(phase == 'train'))
-        metrics = getattr(self, f'{phase}_metrics')(output, targets)
-        self.log(f"{phase}/loss", loss, on_step=False, on_epoch=True, prog_bar=False, sync_dist=True)
-        self.log_dict(metrics, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
-        return {"loss": loss, "output": output, "targets": targets}
